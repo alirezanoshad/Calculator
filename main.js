51875f53389ea4482a4c05aa12c924d8c7785b96
Alireza <iranexplosm@gmail.com>
2024-08-03 23:49:07 +0430

    -calc updated
    -calc got all simple mathematics  possibilities

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 // defining Basic Numbers By User
 let Calculator = {
 
-    number1: 100,
-    number2: 25,
+    numberA: 100,
+    numberB: 25,
 };
 
 // defining Mathematical Applications
 
-let plus = Calculator.number1 + Calculator.number2,
-    minus = Calculator.number1 - Calculator.number2,
-    division = Calculator.number1 / Calculator.number2,
-    secondDivision =  Calculator.number2 / Calculator.number1,
-    multiplication = Calculator.number1 * Calculator.number2;
+let plus = Calculator.numberA + Calculator.numberB,
+    minus = Calculator.numberA - Calculator.numberB,
+    secondMinus = Calculator.numberB - Calculator.numberA,
+    division = Calculator.numberA / Calculator.numberB,
+    secondDivision =  Calculator.numberB / Calculator.numberA,
+    multiplication = Calculator.numberA * Calculator.numberB;
 
 // showing Mathematical Applications Results To The Audience  
-console.log("Basic Numbers",Calculator.number1 , Calculator.number2 );
+console.log("Basic Numbers",Calculator.numberA , Calculator.numberB );
 console.log("Plus",plus );
-console.log("Minus",minus );
-console.log("Division num1 divided by num2",division );
-console.log("Division num2 divided by num1",secondDivision );
+console.log("Minus(numA - numB)",minus );
+console.log("Minus(numB - numA)",secondMinus );
+console.log("Division numA divided by numB",division );
+console.log("Division numB divided by numA",secondDivision );
 console.log("Times",multiplication );
 
 
